726b8a5c921df203de29d02796784943ca0663ab
Joshua Cook <joshua.cook@databricks.com>
2020-09-03 12:25:40 -0700

    add the github repo to the run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,7 @@
 export MLFLOW_TRACKING_URI=databricks
 export EXPERIMENT_NAME=/Users/joshua.cook@databricks.com/building-deploying
 export EXPERIMENT_ID=976445620797865
+export GITHUB_REPO=https://github.com/databricks-edu/Deploying-ML-Models-At-Scale
 mlflow run . \
   --backend databricks \
   --backend-config data/cluster.json \
@@ -16,7 +17,3 @@ mlflow run . \
   --param-list penalty=l1 \
   --param-list max_iter=10000 \
   --param-list experiment_name=$EXPERIMENT_NAME
-
-1. Writing Software for Experiment Tracking
-1. Production-Grade ML Software with Databricks
-1. 
