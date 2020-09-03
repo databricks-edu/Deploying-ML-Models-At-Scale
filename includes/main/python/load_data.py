786ec427e47015797a97214e11bb1c54ce90272d
Joshua Cook <joshua.cook@databricks.com>
2020-09-02 17:09:15 -0700

    handle dbutils

--- includes/main/python/load_data.py
+++ includes/main/python/load_data.py
@@ -2,6 +2,9 @@ from pyspark.sql import SparkSession
 from urllib.request import urlretrieve
 import click
 import os
+import mlflow.utils
+
+dbutils = mlflow.utils._get_dbutils()
 
 def retrieve_data(file: str, landing_path: str) -> bool:
     """Download file from remote location to driver. Move from driver to DBFS."""
@@ -14,20 +17,20 @@ def retrieve_data(file: str, landing_path: str) -> bool:
     dbutils.fs.mv(driverPath, dbfsPath)
     return True
 
-def load_delta_table(spark: SparkSession, filename: str,
+def load_delta_table(spark: SparkSession, file_name: str,
                      landing_path: str, delta_table_path: str) -> bool:
     "Load a parquet file as a Delta table."
-    parquet_df = spark.read.format("parquet").load(landing_path + filename)
+    parquet_df = spark.read.format("parquet").load(landing_path + file_name)
     parquet_df.write.format("delta").save(delta_table_path)
     return True
 
 @click.command(help="process a remote file to a users dbacademy directory")
-@click.option("--filename", help="the name of the remote file")
+@click.option("--file-name", help="the name of the remote file")
 @click.option("--kind", help="event|user")
 @click.option("--username", help="username unique to dbacademy on this workspace")
-def load_data(filename: str, kind: str, username: str) -> bool:
+def load_data(file_name: str, kind: str, username: str) -> bool:
 
-    projectPath     = f"/dbacademy/{username}/mlmodels/profile/"
+    projectPath     = f"dbfs:/dbacademy/{username}/mlmodels/profile/"
     landing_path    = projectPath + "landing/"
     silverDailyPath = projectPath + "daily/"
     dimUserPath     = projectPath + "users/"
@@ -41,8 +44,8 @@ def load_data(filename: str, kind: str, username: str) -> bool:
     else:
         raise ArgumentError("`path` variable must be one of `event` or `user`.")
 
-    retrieve_data(filename, landing_path)
-    load_delta_table(spark, filename, landing_path, table_path)
+    retrieve_data(file_name, landing_path)
+    load_delta_table(spark, file_name, landing_path, table_path)
 
 if __name__ == '__main__':
     load_data()
